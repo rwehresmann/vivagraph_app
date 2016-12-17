93053256b4302ee5628637abd30b520b742e3f1c
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2016-12-17 20:19:19 +0000

    Generate model exercise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161217201716) do
+ActiveRecord::Schema.define(version: 20161217201900) do
+
+  create_table "exercises", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
