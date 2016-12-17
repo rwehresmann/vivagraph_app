066c46a28d01203f075b4573ac3c7c2cdbc0cf9f
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2016-12-17 23:53:31 +0000

    Give some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+names = ["Indiana Jones", "Oliver Twist", "John Galt", "Lisbet Salander", "Ceiwyn of Powys"]
+names.each { |n| User.find_or_create_by(name: n) }
+
+10.times { |i| Exercise.find_or_create_by(name: "exercise_#{i}") }
+
+User.all.each do |u|
+  Exercise.all.each do |e|
+    correct = [true, false].sample
+
+    Answer.create!(correct: correct, user_id: u.id, exercise_id: e.id)
+  end
+end
