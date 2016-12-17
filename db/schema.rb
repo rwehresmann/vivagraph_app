64c250e5fb59109b2f6d939c31b7ee66ee411419
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2016-12-17 20:20:51 +0000

    Generate model answer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161217201900) do
+ActiveRecord::Schema.define(version: 20161217202033) do
+
+  create_table "answers", force: :cascade do |t|
+    t.boolean  "correct"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "exercises", force: :cascade do |t|
     t.string   "name"
