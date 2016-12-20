13c5f909f29e7e124623d0cd2a1edacf8a9eb56a
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2016-12-20 00:42:10 +0000

    Add new data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-names = ["Indiana Jones", "Oliver Twist", "John Galt", "Lisbet Salander", "Ceiwyn of Powys"]
+names = ["Alan Turing", "Marie Curie", "Margaret Hamilton", "Albert Einstein", "Kip Thorne"]
 names.each { |n| User.find_or_create_by(name: n) }
 
 10.times { |i| Exercise.find_or_create_by(name: "exercise_#{i}") }
